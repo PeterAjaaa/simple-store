a7f760273c84534dcf0d195e086fc642286015f2
Ferdinand Lanvino <frdnndvn106@gmail.com>
2024-09-27 14:51:02 +0700

    Refactor form inputs in create.blade.php

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -18,31 +18,27 @@
                 <div class="row">
                     <div class="col-12">
                         <label class="form-label" for="name">Name</label>
-                        <input class="form-control" type="text" name="name" id="name" value="{{ old('name') }}">
+                        <input class="form-control" type="text" name="name" id="name"">
                     </div>
                     <div class="col-12">
                         <label class="form-label" for="description">Description</label>
-                        <textarea class="form-control" name="description" id="description" rows="3">{{ old('description') }}</textarea>
+                        <textarea class="form-control" name="description" id="description" rows="3"></textarea>
                     </div>
                     <div class="col-6">
                         <label class="form-label" for="retail_price">Retail Price</label>
-                        <input class="form-control" type="number" name="retail_price" id="retail_price"
-                            value="{{ old('retail_price') }}">
+                        <input class="form-control" type="number" name="retail_price" id="retail_price">
                     </div>
                     <div class="col-6">
                         <label class="form-label" for="wholesale_price">Wholesale Price</label>
-                        <input class="form-control" type="number" name="wholesale_price" id="wholesale_price"
-                            value="{{ old('wholesale_price') }}">
+                        <input class="form-control" type="number" name="wholesale_price" id="wholesale_price">
                     </div>
                     <div class="col-6">
                         <label class="form-label" for="min_wholesale_qty">Min Wholesale Qty.</label>
-                        <input class="form-control" type="number" name="min_wholesale_qty" id="min_wholesale_qty"
-                            value="{{ old('min_wholesale_qty') }}">
+                        <input class="form-control" type="number" name="min_wholesale_qty" id="min_wholesale_qty">
                     </div>
                     <div class="col-6">
                         <label class="form-label" for="quantity">Quantity</label>
-                        <input class="form-control" type="number" name="quantity" id="quantity"
-                            value="{{ old('quantity') }}">
+                        <input class="form-control" type="number" name="quantity" id="quantity">
                     </div>
 
                     <button class="btn btn-primary mt-3" type="submit">Add</button>
